fbdaed42e25f644cae314787db4a422cdc2d6388
Czerwiec <czerrwiec@gmail.com>
2022-12-20 16:37:35 +0100

    Added shop and single-product site in html, and some styling

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,9 @@ const hamburger = document.querySelector('.hamburger-container');
 const stick = document.querySelector('.stick');
 const navbar = document.querySelector('.navbar');
 const cartIcon = document.querySelector('.cart-icon');
+const MainImg = document.getElementById('MainImg')
+const smallImg = document.querySelectorAll('.small-img') 
+const productBox = document.querySelectorAll('.product-box')
 
 hamburger.addEventListener('click', () => {
 	stick.classList.toggle('trigger');
@@ -12,3 +15,15 @@ hamburger.addEventListener('click', () => {
         cartIcon.style.display = 'block';
     }
 });
+
+smallImg.forEach(item => {
+	item.addEventListener('click', () => {
+		MainImg.src = item.src
+	})
+})
+
+productBox.forEach(item => {
+	item.addEventListener('click', () => {
+		window.location.href = 'single-product.html'
+	})
+})
